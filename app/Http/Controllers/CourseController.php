cfc67b8dccc4b004d5798ccd826155009feacd55
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2023-03-01 11:31:24 -0300

    Trt catch in CourseController

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -6,8 +6,13 @@ use App\DataTransferObjects\CourseDTO;
 use App\Http\Requests\CourseFormRequest;
 use App\Http\Resources\CourseResource;
 use App\Repositories\CoursesRepository;
+use Error;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use PDOException;
 
 class CourseController extends Controller
 {
@@ -18,11 +23,33 @@ class CourseController extends Controller
      */
     public function index(): JsonResponse
     {
-        $courses = $this->repository->all();
-
-        return response()->json([
-            'data' => CourseResource::collection($courses)
-        ]);
+        try {
+            $courses = $this->repository->all();
+    
+            return response()->json([
+                'data' => CourseResource::collection($courses)
+            ]);
+        } catch (PDOException $e) {
+            Log::critical("The database is down: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        } catch (Exception|Error $e) {
+            Log::error("Something is wrong: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        }
     }
 
     /**
@@ -30,67 +57,155 @@ class CourseController extends Controller
      */
     public function store(CourseFormRequest $request): JsonResponse
     {
-        $data = CourseDTO::fromArray($request->validated());
-        $course = $this->repository->create($data);
-
-        return response()->json(['data' => new CourseResource($course)], Response::HTTP_CREATED);
+        try {
+            $data = CourseDTO::fromArray($request->validated());
+            $course = $this->repository->create($data);
+    
+            return response()->json(['data' => new CourseResource($course)], Response::HTTP_CREATED);
+        } catch (PDOException $e) {
+            Log::critical("The database is down: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        } catch (Exception|Error $e) {
+            Log::error("Something is wrong: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(int $id): JsonResponse
+    public function show(string $id): JsonResponse
     {
-        $course = $this->repository->findById($id);
-
-        if (is_null($course)) {
+        try {
+            $course = $this->repository->findById($id);
+    
+            if (is_null($course)) {
+                return response()->json(
+                    ['message' => 'Course not found.'],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+    
+            return response()->json([
+                'data' => new CourseResource($course)
+            ]);
+        } catch (PDOException $e) {
+            Log::critical("The database is down: {$e->getMessage()}");
             return response()->json(
-                ['message' => 'Course not found.'],
-                Response::HTTP_NOT_FOUND
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        } catch (Exception|Error $e) {
+            Log::error("Something is wrong: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
             );
         }
-
-        return response()->json([
-            'data' => new CourseResource($course)
-        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(CourseFormRequest $request, int $id): JsonResponse
+    public function update(CourseFormRequest $request, string $id): JsonResponse
     {
-        $course = $this->repository->findById($id);
-        
-        if (is_null($course)) {
+        try { 
+            $course = $this->repository->findById($id);
+            
+            if (is_null($course)) {
+                return response()->json(
+                    ['message' => 'Course not found.'],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+            
+            $data = CourseDTO::fromArray($request->validated());
+            $this->repository->update($course, $data);
+    
+            return response()->json(['data' => new CourseResource($course)]);
+        } catch (PDOException $e) {
+            Log::critical("The database is down: {$e->getMessage()}");
             return response()->json(
-                ['message' => 'Course not found.'],
-                Response::HTTP_NOT_FOUND
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        } catch (Exception|Error $e) {
+            Log::error("Something is wrong: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
             );
         }
-        
-        $data = CourseDTO::fromArray($request->validated());
-        $this->repository->update($course, $data);
-
-        return response()->json(['data' => new CourseResource($course)]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(int $id): JsonResponse
+    public function destroy(string $id): JsonResponse
     {
-        $course = $this->repository->findById($id);
+        try {
+            $course = $this->repository->findById($id);
 
-        if (is_null($course)) {
+            if (is_null($course)) {
+                return response()->json(
+                    ['message' => 'Course not found.'],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+
+            $this->repository->delete($course);
+
+            return response()->json();
+        } catch (PDOException $e) {
+            Log::critical("The database is down: {$e->getMessage()}");
             return response()->json(
-                ['message' => 'Course not found.'],
-                Response::HTTP_NOT_FOUND
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
+            );
+        } catch (Exception|Error $e) {
+            Log::error("Something is wrong: {$e->getMessage()}");
+            return response()->json(
+                [
+                    'error' => [
+                        'message' => 'Something is wrong now. Try again in a few moments.'
+                    ]
+                ],
+                Response::HTTP_BAD_GATEWAY
             );
         }
-
-        $this->repository->delete($course);
-
-        return response()->json();
     }
 }
